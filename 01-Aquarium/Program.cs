f546b5e2ced0db418492978e634fbc0597d63d24
georgidelchev <alienguymc@gmail.com>
2020-03-16 16:41:44 +0200

    update

--- 01-Aquarium/Program.cs
+++ 01-Aquarium/Program.cs
@@ -1,27 +1,14 @@
 ﻿using System;
 
-namespace Aquarium
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //input data
-            int lenght = int.Parse(Console.ReadLine());
-            int width = int.Parse(Console.ReadLine());
-            int hight = int.Parse(Console.ReadLine());
-            double percent = double.Parse(Console.ReadLine());
+            double a = 12.556;
 
-            //calculations
-            double area = lenght * width * hight;
-            double capacity = area * 0.001;
-            double sand = capacity * percent / 100;
-            double remaining = capacity - sand;
-
-            double result = 100 / 8;
-
-            //print
-            Console.WriteLine($"{remaining:F3}");
+            Console.WriteLine("{0:f2}",a);
         }
     }
 }

6d7b4034a68d5a401b2e6de52b41357ac50fc671
georgidelchev <alienguymc@gmail.com>
2020-03-16 16:43:02 +0200

    upload

---
+++ 01-Aquarium/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aquarium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int hight = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double area = lenght * width * hight;
+            double capacity = area * 0.001;
+            double sand = capacity * percent / 100;
+            double remaining = capacity - sand;
+
+            Console.WriteLine($"{remaining:F3}");
+        }
+    }
+}

77d0b64acebc2346332cc25acf43e1f1bc2b18b8
georgidelchev <alienguymc@gmail.com>
2020-03-17 22:35:05 +0200

    updated variables

--- 01-Aquarium/Program.cs
+++ 01-Aquarium/Program.cs
@@ -6,18 +6,17 @@ namespace Aquarium
     {
         static void Main(string[] args)
         {
-
             int lenght = int.Parse(Console.ReadLine());
             int width = int.Parse(Console.ReadLine());
             int height = int.Parse(Console.ReadLine());
             double percent = double.Parse(Console.ReadLine());
 
-            double area = lenght * width * hight;
-            double capacity = area * 0.001;
-            double sand = capacity * percent / 100;
-            double remaining = capacity - sand;
+            int aquariumVolume = lenght * width * height;
+            double totalLitersCanAquariumTake = aquariumVolume * 0.001;
+            double percentOfTookArea = percent * 0.01;
+            double realLitersThatCanTook = totalLitersCanAquariumTake * (1 - percentOfTookArea);
 
-            Console.WriteLine($"{remaining:F3}");
+            Console.WriteLine($"{realLitersThatCanTook:f3}");
         }
     }
 }

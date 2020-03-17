ebd6b1cc8473072097c6b33e046ed473f989223c
georgidelchev <alienguymc@gmail.com>
2020-03-17 20:24:12 +0200

    up

--- 02-YardGreening/Program.cs
+++ 02-YardGreening/Program.cs
@@ -6,15 +6,17 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            double squareMeters = double.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
 
-            double pricePerSquareMeter = squareMeters * 7.61;
-            double discountPrice = pricePerSquareMeter * 0.18;
+            int aquariumVolume = lenght * width * height;
+            double totalLitersCanAquariumTake = aquariumVolume * 0.001;
+            double percentOfTookArea = percent * 0.1;
+            double realLitersThatCanTook = totalLitersCanAquariumTake * (1 - percentOfTookArea);
 
-            double finalPrice = pricePerSquareMeter - discountPrice;
-
-            Console.WriteLine($"The final price is: {finalPrice:f2} lv.");
-            Console.WriteLine($"The discount is: {discountPrice:f2} lv.");
+            Console.WriteLine($"{realLitersThatCanTook:f3}");
         }
     }
 }

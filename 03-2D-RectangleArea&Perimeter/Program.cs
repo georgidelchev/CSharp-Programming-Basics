6d9c677bcaf13d9ed34b819d21e533ddd5d13889
georgidelchev <alienguymc@gmail.com>
2020-03-17 22:16:46 +0200

    created another task - 2D Rectangle Area and Perimeter

---
+++ 03-2D-RectangleArea&Perimeter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SoftUniTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double length = Math.Abs(x1 - x2);
+            double width = Math.Abs(y1 - y2);
+
+            double area = length * width;
+            double perimeter = 2 * (length + width);
+
+            Console.WriteLine($"{area:f2}");
+            Console.WriteLine($"{perimeter:f2}");
+        }
+    }
+}

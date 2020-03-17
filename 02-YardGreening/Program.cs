ccbdbd5411c7c4a744f7a60d994ad10eec06d66d
georgidelchev <alienguymc@gmail.com>
2020-03-17 03:12:39 +0200

    Second application - Yard Greening

---
+++ 02-YardGreening/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squareMeters = double.Parse(Console.ReadLine());
+
+            double pricePerSquareMeter = squareMeters * 7.61;
+            double discountPrice = pricePerSquareMeter * 0.18;
+
+            double finalPrice = pricePerSquareMeter - discountPrice;
+
+            Console.WriteLine($"The final price is: {finalPrice:f2} lv.");
+            Console.WriteLine($"The discount is: {discountPrice:f2} lv.");
+        }
+    }
+}

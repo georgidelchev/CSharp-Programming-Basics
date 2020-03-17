a7c804881b4c58a0fc1c069e98832895402a3af4
georgidelchev <alienguymc@gmail.com>
2020-03-17 22:36:41 +0200

    updated the task

--- 02-YardGreening/Program.cs
+++ 02-YardGreening/Program.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 
-namespace ConsoleApp10
+namespace YardGreening
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int lenght = int.Parse(Console.ReadLine());
-            int width = int.Parse(Console.ReadLine());
-            int height = int.Parse(Console.ReadLine());
-            double percent = double.Parse(Console.ReadLine());
+            double squareMeters = double.Parse(Console.ReadLine());
 
-            int aquariumVolume = lenght * width * height;
-            double totalLitersCanAquariumTake = aquariumVolume * 0.001;
-            double percentOfTookArea = percent * 0.1;
-            double realLitersThatCanTook = totalLitersCanAquariumTake * (1 - percentOfTookArea);
+            double pricePerSquareMeter = squareMeters * 7.61;
+            double discountPrice = pricePerSquareMeter * 0.18;
 
-            Console.WriteLine($"{realLitersThatCanTook:f3}");
+            double finalPrice = pricePerSquareMeter - discountPrice;
+
+            Console.WriteLine($"The final price is: {finalPrice:f2} lv.");
+            Console.WriteLine($"The discount is: {discountPrice:f2} lv.");
         }
     }
 }

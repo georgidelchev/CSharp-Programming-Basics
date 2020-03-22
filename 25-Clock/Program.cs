73201cd76a6102b7bf7f0b16e606acd44ef69b19
georgidelchev <alienguymc@gmail.com>
2020-03-22 17:40:41 +0200

    upload

---
+++ 25-Clock/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeOne = int.Parse(Console.ReadLine());
+            int timeTwo = int.Parse(Console.ReadLine());
+            int timeThree = int.Parse(Console.ReadLine());
+
+            int totalTime = timeOne + timeTwo+timeThree;
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:{seconds:d2}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+        }
+    }
+}

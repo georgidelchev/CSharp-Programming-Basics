82ed5377065f1271529a26f7632d6b8bcbd96998
georgidelchev <alienguymc@gmail.com>
2020-03-25 02:57:18 +0200

    upload

---
+++ 42-ClassOfAnimal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AnimalClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animalType = Console.ReadLine();
+
+            switch (animalType)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+
+        }
+    }
+}

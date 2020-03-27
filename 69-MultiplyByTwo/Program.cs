bb1a2692679a5c784a8054354ac456f95b8f0b2a
georgidelchev <alienguymc@gmail.com>
2020-03-27 03:37:35 +0200

    upload

---
+++ 69-MultiplyByTwo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplyBy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numbers = double.Parse(Console.ReadLine());
+
+            
+
+            while (numbers >= 0)
+            { 
+                Console.WriteLine($"Result: {(numbers*2):f2}");
+                numbers = double.Parse(Console.ReadLine());              
+            }
+            Console.WriteLine("Negative number!");
+        }
+    }
+}

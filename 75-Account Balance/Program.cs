77700d36329d071e9bdeae68a2b0cdf365f2cdde
georgidelchev <alienguymc@gmail.com>
2020-03-30 19:13:39 +0300

    upload

---
+++ 75-Account Balance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int paymentsCount = int.Parse(Console.ReadLine());
+
+            int payments = 0;
+            double totalSum = 0;
+            while (payments < paymentsCount)
+            {
+                double moneyIn = double.Parse(Console.ReadLine());
+                if (moneyIn < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {moneyIn:f2}");
+                totalSum += moneyIn;
+                payments++;
+            }
+            Console.WriteLine($"Total: {totalSum:f2}");
+        }
+    }
+}

31137230b58939a809e0a8b22dc4c8a74ab08874
georgidelchev <alienguymc@gmail.com>
2020-03-30 18:54:30 +0300

    upload

---
+++ 73-Password/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user = Console.ReadLine();
+            string pass = Console.ReadLine();
+
+            string passwordInput = Console.ReadLine();
+            while (passwordInput != pass)
+            {
+                passwordInput = Console.ReadLine();
+            }
+            Console.WriteLine($"Welcome {user}");
+        }
+    }
+}

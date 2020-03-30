a7eae199535b86ac29e22ca914b17b2603ad99a9
georgidelchev <alienguymc@gmail.com>
2020-03-31 01:50:19 +0300

    upload

---
+++ 82-AverageNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (int i = 0; i < number; i++)
+            {
+                double numbers = double.Parse(Console.ReadLine());
+                sum += numbers;
+            }
+            Console.WriteLine($"{sum/number:f2}");
+        }
+    }
+}

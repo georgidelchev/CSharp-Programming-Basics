d0ed43f52d2274f6b0d081165c219df0507c11c9
georgidelchev <alienguymc@gmail.com>
2020-04-02 00:49:08 +0300

    upload

---
+++ Programming Basics Online Exam - 15 and 16 June 2019/02-LunchBreak/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LunchBreak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int serieTime = int.Parse(Console.ReadLine());
+            int restTime = int.Parse(Console.ReadLine());
+
+            double lunchTime = restTime * 1.00 / 8;
+            double restingTime = restTime * 1.00 / 4;
+
+            double timeLeft = restTime - lunchTime - restingTime;
+
+            if (timeLeft >= serieTime)
+            {
+                double leftTime = timeLeft - serieTime;
+                Console.WriteLine($"You have enough time to watch {name} and left with {Math.Ceiling(leftTime)} minutes free time.");
+            }
+            else if (serieTime > timeLeft)
+            {
+                double timeNeeded = serieTime - timeLeft;
+                Console.WriteLine($"You don't have enough time to watch {name}, you need {Math.Ceiling(timeNeeded)} more minutes.");
+            }
+        }
+    }
+}

a4b60c3d810885da70d7b8ffa3d4059d15dd1e29
georgidelchev <alienguymc@gmail.com>
2020-04-03 03:21:13 +0300

    upload

---
+++ Programming Basics Online Exam - 9 and 10 March 2019/05-TennisRanklist/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TennisRanklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tournamentsCount = int.Parse(Console.ReadLine());
+            int startingPoints = int.Parse(Console.ReadLine());
+
+            double totalPoints = startingPoints;
+            double averagePoints = 0;
+            double percentWon = 0;
+
+            for (int i = 0; i < tournamentsCount; i++)
+            {
+                string stage = Console.ReadLine();
+                if (stage == "W")
+                {
+                    totalPoints = totalPoints + 2000;
+                    averagePoints = averagePoints + 2000;
+                    percentWon++;
+                }
+                else if (stage == "F")
+                {
+                    totalPoints = totalPoints + 1200;
+                    averagePoints = averagePoints + 1200;
+                }
+                else if (stage == "SF")
+                {
+                    totalPoints = totalPoints + 720;
+                    averagePoints = averagePoints + 720;
+                }
+            }
+            averagePoints = Math.Floor(averagePoints / tournamentsCount);
+            Console.WriteLine($"Final points: {totalPoints}");
+            Console.WriteLine($"Average points: {averagePoints}");
+            Console.WriteLine($"{(percentWon / tournamentsCount) * 100:f2}%");
+        }
+    }
+}

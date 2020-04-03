64a8b20f356a74595d8322fb8d1748a5b8543ec4
georgidelchev <alienguymc@gmail.com>
2020-04-03 04:04:54 +0300

    upload

---
+++ Programming Basics Online Exam - 9 and 10 March 2019/06-BasketballTournament/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BasketballTournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tournamentName = Console.ReadLine();
+
+            double ourWinPoints = 0;
+            double ourLosePoints = 0;
+            double totalMatches = 0;
+
+            while (tournamentName != "End of tournaments")
+            {
+                int tournamentMatches = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= tournamentMatches; i++)
+                {
+                    int ourTeamPoints = int.Parse(Console.ReadLine());
+                    int enemyTeamPoints = int.Parse(Console.ReadLine());
+                    if (ourTeamPoints > enemyTeamPoints)
+                    {
+                        ourWinPoints++;
+                        Console.WriteLine($"Game {i} of tournament {tournamentName}: win with {ourTeamPoints - enemyTeamPoints} points.");
+                    }
+                    else if (enemyTeamPoints > ourTeamPoints)
+                    {
+                        Console.WriteLine($"Game {i} of tournament {tournamentName}: lost with {enemyTeamPoints - ourTeamPoints} points.");
+                        ourLosePoints++;
+                    }
+                }
+                totalMatches += tournamentMatches;
+                tournamentName = Console.ReadLine();
+            }
+            if (tournamentName == "End of tournaments")
+            {
+                Console.WriteLine($"{(ourWinPoints / totalMatches) * 100:f2}% matches win");
+                Console.WriteLine($"{(ourLosePoints / totalMatches) * 100:f2}% matches lost");
+            }
+        }
+    }
+}

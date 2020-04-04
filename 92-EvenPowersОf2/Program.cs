830eb37cd86c5e5c9f481dbf37d9c791a9b42c88
georgidelchev <alienguymc@gmail.com>
2020-04-04 23:18:16 +0300

    upload

---
+++ 92-EvenPowersОf2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EvenPowersOf2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= number; i += 2)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}

8f24173d4bd1f87da0bb65a884ff478a6a4003ab
georgidelchev <alienguymc@gmail.com>
2020-04-04 23:36:19 +0300

    upload

---
+++ 95-SumNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (int i = 0; i < numbers; i++)
+            {
+                int numbersToCount = int.Parse(Console.ReadLine());
+                sum += numbersToCount;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

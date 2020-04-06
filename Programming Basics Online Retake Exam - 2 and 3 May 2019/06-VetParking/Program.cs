9d3fb8ea994113541c73209dbeee845b930b12ab
georgidelchev <alienguymc@gmail.com>
2020-04-07 01:06:23 +0300

    upload

---
+++ Programming Basics Online Retake Exam - 2 and 3 May 2019/06-VetParking/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace VetParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            double tax = 0;
+            double totalSum = 0;
+            for (int i = 1; i <= days; i++)
+            {
+                double sum = 0;
+                for (int j = 1; j <= hours; j++)
+                {
+                    if (i % 2 == 0 && j % 2 != 0)
+                    {
+                        tax = 2.50;
+                        sum += tax;
+                    }
+                    else if (i % 2 != 0 && j % 2 == 0)
+                    {
+                        tax = 1.25;
+                        sum += tax;
+                    }
+                    else
+                    {
+                        tax = 1.00;
+                        sum += tax;
+                    }
+                }
+                totalSum += sum;
+                Console.WriteLine($"Day: {i} - {sum:f2} leva");
+            }
+            Console.WriteLine($"Total: {totalSum:f2} leva");
+        }
+    }
+}

ace4ea078e0620b9cb8fd75ec56b966ce3d9eb0a
georgidelchev <alienguymc@gmail.com>
2020-04-10 21:47:48 +0300

    add files via upload

---
+++ Programming Basics Online Exam - 28 and 29 March 2020/02 - [MountainRun]/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MountainRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentRecord = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timePerMeterInSeconds = double.Parse(Console.ReadLine());
+
+            double neededTime = distanceInMeters * timePerMeterInSeconds;
+            double slowingTime = Math.Floor((distanceInMeters / 50)) * 30;
+            double totalTimeNeeded = neededTime + slowingTime;
+
+            if (totalTimeNeeded >= currentRecord)
+            {
+                double timeNeeded = totalTimeNeeded - currentRecord;
+                Console.WriteLine($"No! He was {timeNeeded:f2} seconds slower.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes! The new record is {totalTimeNeeded:f2} seconds. ");
+            }
+        }
+    }
+}

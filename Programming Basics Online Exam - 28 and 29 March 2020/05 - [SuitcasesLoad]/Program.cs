f9c658da080499f103f134f0a94457c08ace1dd1
georgidelchev <alienguymc@gmail.com>
2020-04-11 00:04:49 +0300

    add files via upload

---
+++ Programming Basics Online Exam - 28 and 29 March 2020/05 - [SuitcasesLoad]/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SuitcasesLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double capacity = double.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                double caseVolume = double.Parse(command);
+                counter++;
+                if (counter != 0 && counter % 3 == 0)
+                {
+                    caseVolume *= 1.1;
+                }
+                
+                if (caseVolume > capacity)
+                {
+                    counter--;
+                    Console.WriteLine($"No more space!");
+                    break;
+                }
+                capacity -= caseVolume;
+
+                command = Console.ReadLine();
+            }
+            
+            if (command == "End")
+            {
+                Console.WriteLine("Congratulations! All suitcases are loaded!");
+            }
+            Console.WriteLine($"Statistic: {counter} suitcases loaded.");
+
+        }
+    }
+}

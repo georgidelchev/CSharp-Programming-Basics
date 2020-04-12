4c94695d7e0f24b7067896cd843bd9adcfff27e8
georgidelchev <alienguymc@gmail.com>
2020-04-12 03:53:36 +0300

    add files via upload

---
+++ 122 - [ReadText]/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReadText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            while (word != "Stop")
+            {
+                word = Console.ReadLine();
+            }
+        }
+    }
+}

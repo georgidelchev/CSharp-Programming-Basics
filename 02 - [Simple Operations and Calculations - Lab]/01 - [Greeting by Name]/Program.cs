a006136fefa8bae402e0f6973ab22b640daca63c
georgidelchev <alienguymc@gmail.com>
2020-04-26 21:04:53 +0300

    add files via upload

---
+++ 02 - [Simple Operations and Calculations - Lab]/01 - [Greeting by Name]/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GreetingByName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}

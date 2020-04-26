da80ef36ffe5f41313446df9bd5e6b8f247bc259
georgidelchev <alienguymc@gmail.com>
2020-04-26 21:21:28 +0300

    add files via upload

---
+++ 02 - [Simple Operations and Calculations - Lab]/06 - [Pet Shop]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogsCount = int.Parse(Console.ReadLine());
+            int otherAnimalsCount = int.Parse(Console.ReadLine());
+
+            double totalPriceForDogsFood = dogsCount * 2.50;
+            double totalPriceForOtherAnimalsFood = otherAnimalsCount * 4.00;
+
+            double totalPrice = totalPriceForDogsFood + totalPriceForOtherAnimalsFood;
+            Console.WriteLine($"{totalPrice:f2} lv.");
+        }
+    }
+}

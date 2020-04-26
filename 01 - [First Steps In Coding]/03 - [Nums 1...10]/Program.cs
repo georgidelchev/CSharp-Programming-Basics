c50040b834a71e35f65809b702b8e2e2e495920d
georgidelchev <alienguymc@gmail.com>
2020-04-26 20:50:55 +0300

    add files via upload

---
+++ 01 - [First Steps In Coding]/03 - [Nums 1...10]/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nums1to10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

056c4248f14085f4c0493a0f2097fc950481af00
georgidelchev <alienguymc@gmail.com>
2020-04-26 21:18:46 +0300

    add files via upload

---
+++ 02 - [Simple Operations and Calculations - Lab]/05 - [Projects Creation]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjectsCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string archName = Console.ReadLine();
+            int projectsNumber = int.Parse(Console.ReadLine());
+
+            int neededHours = projectsNumber * 3;
+            Console.WriteLine($"The architect {archName} will need {neededHours} hours to complete {projectsNumber} project/s.");
+
+        }
+    }
+}

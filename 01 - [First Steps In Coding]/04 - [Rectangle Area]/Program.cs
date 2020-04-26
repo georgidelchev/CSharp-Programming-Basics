4e6f8ff27ac1e3f914c0991ec96c02f354876407
georgidelchev <alienguymc@gmail.com>
2020-04-26 20:52:28 +0300

    add files via upload

---
+++ 01 - [First Steps In Coding]/04 - [Rectangle Area]/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+            double rectangleArea = sideA * sideB;
+            Console.WriteLine(rectangleArea);
+        }
+    }
+}

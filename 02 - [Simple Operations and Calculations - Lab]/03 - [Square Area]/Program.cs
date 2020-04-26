658455c16fc56ea0c50321fcbf8f22b977910dac
georgidelchev <alienguymc@gmail.com>
2020-04-26 21:12:28 +0300

    add files via upload

---
+++ 02 - [Simple Operations and Calculations - Lab]/03 - [Square Area]/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double squareArea = number * number;
+            Console.WriteLine(squareArea);
+        }
+    }
+}

3c21c2849b5115841f6f41b71031ccb8a32c7502
georgidelchev <alienguymc@gmail.com>
2020-04-27 10:31:03 +0300

    add files via upload

---
+++ 04 - [Simple Operations and Calculations - More Exercises]/03 - [Celsius to Fahrenheit]/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CelsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deg = double.Parse(Console.ReadLine());
+            double far = (deg * 1.80) + 32;
+            Console.WriteLine($"{far:f2}");
+        }
+    }
+}

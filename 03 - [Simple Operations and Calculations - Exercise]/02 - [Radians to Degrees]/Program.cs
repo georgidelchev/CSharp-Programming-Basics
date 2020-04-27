d9b9a8bbedd50ec56e2d3f7f9929a0f3db5eb85d
georgidelchev <alienguymc@gmail.com>
2020-04-27 10:21:04 +0300

    add files via upload

---
+++ 03 - [Simple Operations and Calculations - Exercise]/02 - [Radians to Degrees]/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RadiansToDegrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+            double convertedToDegrees = radians * 180 / Math.PI;
+            Console.WriteLine(Math.Round(convertedToDegrees));
+        }
+    }
+}

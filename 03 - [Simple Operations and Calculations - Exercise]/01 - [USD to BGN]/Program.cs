351d785c19a7dc19bca8cb8b9449fc56d640a3de
georgidelchev <alienguymc@gmail.com>
2020-04-27 10:17:36 +0300

    add files via upload

---
+++ 03 - [Simple Operations and Calculations - Exercise]/01 - [USD to BGN]/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UsdToBgn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double dollars = double.Parse(Console.ReadLine());
+            double convertedToLevs = dollars * 1.79549;
+            Console.WriteLine($"{convertedToLevs:f2}");
+        }
+    }
+}

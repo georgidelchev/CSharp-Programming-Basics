46254c78982f69e464da4df2532b2b213021a33b
georgidelchev <alienguymc@gmail.com>
2020-04-27 10:29:53 +0300

    add files via upload

---
+++ 04 - [Simple Operations and Calculations - More Exercises]/02 - [Triangle Area]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double triangleArea = side * height / 2;
+
+            Console.WriteLine($"{triangleArea:f2}");
+        }
+    }
+}

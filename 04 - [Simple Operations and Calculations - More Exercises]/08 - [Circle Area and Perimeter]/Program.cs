9d0bde508668a84ac407efdb829b6bf703552e22
georgidelchev <alienguymc@gmail.com>
2020-04-27 10:32:15 +0300

    add files via upload

---
+++ 04 - [Simple Operations and Calculations - More Exercises]/08 - [Circle Area and Perimeter]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CircleAreaAndPerimeter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            double circleArea = Math.PI * r * r;
+            double circlePerimeter = 2 * Math.PI * r;
+
+            Console.WriteLine($"{circleArea:f2}");
+            Console.WriteLine($"{circlePerimeter:f2}");
+        }
+    }
+}

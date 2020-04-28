b93f7dbc43d4309f736739f856a2ac4680361c07
georgidelchev <alienguymc@gmail.com>
2020-04-28 16:05:14 +0300

    add files via upload

---
+++ 10 - [Nested Conditional Statements - More Exercises]/01 - [Match Tickets]/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace MatchTickets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string category = Console.ReadLine();
+            int peopleNumber = int.Parse(Console.ReadLine());
+
+            double transportPrice = 0;
+            double vipTicket = 499.99;
+            double normalTicket = 249.99;
+            double totalSumForTickets = 0;
+            double totalSum = 0;
+
+            if (peopleNumber >= 1 && peopleNumber <= 4)
+            {
+                transportPrice = 0.75 * budget;
+            }
+            else if (peopleNumber >= 5 && peopleNumber <= 9)
+            {
+                transportPrice = 0.60 * budget;
+            }
+            else if (peopleNumber >= 10 && peopleNumber <= 24)
+            {
+                transportPrice = 0.50 * budget;
+            }
+            else if (peopleNumber >= 25 && peopleNumber <= 49)
+            {
+                transportPrice = 0.40 * budget;
+            }
+            else if (peopleNumber >= 50)
+            {
+                transportPrice = 0.25 * budget;
+            }
+
+            if (category == "VIP")
+            {
+                totalSumForTickets = peopleNumber * vipTicket;
+                
+            }
+            else if (category == "Normal")
+            {
+                totalSumForTickets = peopleNumber * normalTicket;
+
+            }
+
+            totalSum = transportPrice + totalSumForTickets;
+
+            if (budget >= totalSum)
+            {
+                double moneyLeft = budget - totalSum;
+                Console.WriteLine($"Yes! You have {moneyLeft:f2} leva left.");
+            }
+            else if (totalSum > budget)
+            {
+                double moneyNeeded = totalSum - budget;
+                Console.WriteLine($"Not enough money! You need {moneyNeeded:f2} leva.");
+            }
+        }
+    }
+}
---
+++ 10 - [Nested Conditional Statements - More Exercises]/08 - [Numbers From 1 to 10]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NumberFrom1to10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            while (counter <= 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+        }
+    }
+}

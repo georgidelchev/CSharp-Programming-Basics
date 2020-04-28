12275b1e578893db0fc1c47f1e1bee5224239187
georgidelchev <alienguymc@gmail.com>
2020-04-28 16:00:42 +0300

    add files via upload

---
+++ 08 - [Conditional Statements Advanced - Lab]/03 - [Personal Titles]/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PersonalTitles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            switch (gender)
+            {
+                case "m":
+                    if (age >= 16)
+                    {
+                        Console.WriteLine("Mr.");
+                    }
+                    else if (age < 16)
+                    {
+                        Console.WriteLine("Master");
+                    }
+                    break;
+                case "f":
+                    if (age >= 16)
+                    {
+                        Console.WriteLine("Ms.");
+                    }
+                    else if (age < 16)
+                    {
+                        Console.WriteLine("Miss");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ 08 - [Conditional Statements Advanced - Lab]/08 - [Fruit Shop]/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace FruitShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double qty = double.Parse(Console.ReadLine());
+
+            double totalSum = 0;
+
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            totalSum = qty * 2.50;
+                            break;
+                        case "apple":
+                            totalSum = qty * 1.20;
+                            break;
+                        case "orange":
+                            totalSum = qty * 0.85;
+                            break;
+                        case "grapefruit":
+                            totalSum = qty * 1.45;
+                            break;
+                        case "kiwi":
+                            totalSum = qty * 2.70;
+                            break;
+                        case "pineapple":
+                            totalSum = qty * 5.50;
+                            break;
+                        case "grapes":
+                            totalSum = qty * 3.85;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            totalSum = qty * 2.70;
+                            break;
+                        case "apple":
+                            totalSum = qty * 1.25;
+                            break;
+                        case "orange":
+                            totalSum = qty * 0.90;
+                            break;
+                        case "grapefruit":
+                            totalSum = qty * 1.60;
+                            break;
+                        case "kiwi":
+                            totalSum = qty * 3.00;
+                            break;
+                        case "pineapple":
+                            totalSum = qty * 5.60;
+                            break;
+                        case "grapes":
+                            totalSum = qty * 4.20;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+            if (totalSum > 0)
+            {
+                Console.WriteLine($"{totalSum:f2}");
+
+            }
+        }
+    }
+}

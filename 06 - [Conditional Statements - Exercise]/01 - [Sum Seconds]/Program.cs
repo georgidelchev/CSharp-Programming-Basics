0e798c79c112df5ab25923caab2c6bd5db4210d3
georgidelchev <alienguymc@gmail.com>
2020-04-28 15:39:12 +0300

    add files via upload

---
+++ 06 - [Conditional Statements - Exercise]/01 - [Sum Seconds]/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeOne = int.Parse(Console.ReadLine());
+            int timeTwo = int.Parse(Console.ReadLine());
+            int timeThree = int.Parse(Console.ReadLine());
+
+            int totalTime = timeOne + timeThree + timeTwo;
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:{seconds:d2}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+        }
+    }
+}

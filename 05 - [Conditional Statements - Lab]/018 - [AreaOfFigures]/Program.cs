6ba93543fd6226c3ef8d043cc333e95014a17f7b
georgidelchev <alienguymc@gmail.com>
2020-04-28 15:27:40 +0300

    add files via upload

---
+++ 05 - [Conditional Statements - Lab]/018 - [AreaOfFigures]/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            switch (figure)
+            {
+                case "square":
+                    double a = double.Parse(Console.ReadLine());
+                    double squareArea = a * a;
+                    Console.WriteLine($"{squareArea:f3}");
+                    break;
+                case "rectangle":
+                    double sideA = double.Parse(Console.ReadLine());
+                    double sideB = double.Parse(Console.ReadLine());
+                    double rectanbleArea = sideA * sideB;
+                    Console.WriteLine($"{rectanbleArea:f3}");
+                    break;
+                case "circle":
+                    double r = double.Parse(Console.ReadLine());
+                    double circleArea = Math.PI * r * r;
+                    Console.WriteLine($"{circleArea:f3}");
+                    break;
+                case "triangle":
+                    double triangleSideA = double.Parse(Console.ReadLine());
+                    double triangleHeight = double.Parse(Console.ReadLine());
+                    double triangleArea = triangleSideA * triangleHeight / 2;
+                    Console.WriteLine($"{triangleArea:f3}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}

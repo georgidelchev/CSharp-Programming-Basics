edfcd24be4a707f5064b3afaec0f5458be02842a
georgidelchev <alienguymc@gmail.com>
2020-04-28 15:40:10 +0300

    add files via upload

---
+++ 06 - [Conditional Statements - Exercise]/04 - [MetricConverter]/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valueToConvert = double.Parse(Console.ReadLine());
+            string metricIn = Console.ReadLine();
+            string metricOut = Console.ReadLine();
+
+            double converted = 0;
+
+            if (metricIn == "m")
+            {
+                if (metricOut == "cm")
+                {
+                    converted = valueToConvert * 100;
+                }
+                if (metricOut == "mm")
+                {
+                    converted = valueToConvert * 1000;
+                }
+            }
+            if (metricIn == "cm")
+            {
+                if (metricOut == "m")
+                {
+                    converted = valueToConvert * 0.01;
+                }
+                if (metricOut == "mm")
+                {
+                    converted = valueToConvert * 10;
+                }
+            }
+            if (metricIn == "mm")
+            {
+                if (metricOut == "cm")
+                {
+                    converted = valueToConvert * 0.1;
+                }
+                if (metricOut == "m")
+                {
+                    converted = valueToConvert * 0.001;
+                }
+            }
+            Console.WriteLine($"{converted:f3}");
+        }
+    }
+}
---
+++ 06 - [Conditional Statements - Exercise]/08 - [Scholarship]/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Scholarship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            double averageSuccess = double.Parse(Console.ReadLine());
+            double minimalSalary = double.Parse(Console.ReadLine());
+
+            double scholarship = 0;
+            double scolarshipExcellent = 0;
+
+            if (income <= minimalSalary && averageSuccess > 4.50 && averageSuccess < 5.50)
+            {
+                scholarship = Math.Floor(minimalSalary * 0.35);
+                Console.WriteLine($"You get a Social scholarship {scholarship} BGN");
+            }
+            else if (averageSuccess >= 5.50)
+            {
+                scolarshipExcellent = Math.Floor(averageSuccess * 25);
+                Console.WriteLine($"You get a scholarship for excellent results {scolarshipExcellent} BGN");
+            }
+            else
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+        }
+    }
+}

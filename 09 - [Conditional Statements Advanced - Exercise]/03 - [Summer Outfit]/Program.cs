f1078eb2708b4d253cf9d7ef4417b275049e019d
georgidelchev <alienguymc@gmail.com>
2020-04-28 16:02:50 +0300

    add files via upload

---
+++ 09 - [Conditional Statements Advanced - Exercise]/03 - [Summer Outfit]/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SummerOutfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int deg = int.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+
+            string outfit = string.Empty;
+            string shoes = string.Empty;
+
+
+            switch (time)
+            {
+                case "Morning":
+                    if (10 <= deg && deg <= 18)
+                    {
+                        outfit = "Sweatshirt";
+                        shoes = "Sneakers";
+                    }
+                    else if (18 < deg && deg <= 24)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (deg >= 25)
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    break;
+                case "Afternoon":
+                    if (10 <= deg && deg <= 18)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (18 < deg && deg <= 24)
+                    {
+                        outfit = "T-Shirt";
+                        shoes = "Sandals";
+                    }
+                    else if (deg >= 25)
+                    {
+                        outfit = "Swim Suit";
+                        shoes = "Barefoot";
+                    }
+                    break;
+                case "Evening":
+                    if (10 <= deg && deg <= 18)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (18 < deg && deg <= 24)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    else if (deg >= 25)
+                    {
+                        outfit = "Shirt";
+                        shoes = "Moccasins";
+                    }
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine($"It's {deg} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
---
+++ 09 - [Conditional Statements Advanced - Exercise]/05 - [Fishing Boat]/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace FishingBoat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermenCount = int.Parse(Console.ReadLine());
+
+            int shipRentAtSpring = 3000;
+            int shipRentAtAutumnAndSummer = 4200;
+            int shipRentAtWinter = 2600;
+
+            double totalSum = 0;
+            double discount = 0;
+
+            switch (season)
+            {
+                case "Spring":
+                    totalSum = shipRentAtSpring;
+                    break;
+                case "Summer":
+                    totalSum = shipRentAtAutumnAndSummer;
+                    break;
+                case "Autumn":
+                    totalSum = shipRentAtAutumnAndSummer;
+                    break;
+                case "Winter":
+                    totalSum = shipRentAtWinter;
+                    break;
+                default:
+                    break;
+            }
+            
+            if (fishermenCount <= 6)
+            {
+                discount = totalSum * 0.10;
+                totalSum = totalSum - discount;
+            }
+            else if (fishermenCount >= 7 && fishermenCount <= 11)
+            {
+                discount = totalSum * 0.15;
+                totalSum = totalSum - discount;
+            }
+            else if (fishermenCount >= 12)
+            {
+                discount = totalSum * 0.25;
+                totalSum = totalSum - discount;
+            }
+            
+            if (fishermenCount % 2 == 0 && season != "Autumn")
+            {
+                double discountTwo = totalSum * 0.05;
+                totalSum = totalSum - discountTwo;
+            }
+
+            if (budget >= totalSum)
+            {
+                double moneyLeft = budget - totalSum;
+                Console.WriteLine($"Yes! You have {moneyLeft:f2} leva left.");
+            }
+            else if (totalSum > budget)
+            {
+                double moneyNeeded = totalSum - budget;
+                Console.WriteLine($"Not enough money! You need {moneyNeeded:f2} leva.");
+            }
+        }
+    }
+}

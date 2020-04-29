9abd75bb8a95e337186f2c2292400e6529cfb725
georgidelchev <alienguymc@gmail.com>
2020-04-29 23:22:05 +0300

    add files via upload

---
+++ [Other Solutions]/15 - [Factorial]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine(factorial);
+        }
+    }
+}
+

e6f6b37b6481e0f98d7bd51621114e231b712de2
georgidelchev <alienguymc@gmail.com>
2020-04-29 23:46:19 +0300

    add files via upload

---
+++ [Other Solutions]/17 - [Check Prime]/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CheckPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+
+            if (number <= 1)
+            {
+                isPrime = false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if ((number % i == 0))
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not prime");
+            }
+
+        }
+    }
+}

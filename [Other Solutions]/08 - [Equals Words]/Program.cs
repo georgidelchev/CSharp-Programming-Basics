a8746540445480898f66ed575417a1e59a22379d
georgidelchev <alienguymc@gmail.com>
2020-04-29 13:22:39 +0300

    add files via upload

---
+++ [Other Solutions]/08 - [Equals Words]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = Console.ReadLine().ToLower();
+            string secondWord = Console.ReadLine().ToLower();
+
+            if (Equals(firstWord,secondWord))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}

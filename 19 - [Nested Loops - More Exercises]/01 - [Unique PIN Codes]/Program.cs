32c1398345137f061f1f63f4e2f7f163462e6831
georgidelchev <alienguymc@gmail.com>
2020-04-29 16:15:33 +0300

    add files via upload

---
+++ 19 - [Nested Loops - More Exercises]/01 - [Unique PIN Codes]/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UniquePins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumberUpperBorder = int.Parse(Console.ReadLine());
+            int secondNumberUpperBorder = int.Parse(Console.ReadLine());
+            int thirdNumberUpperBorder = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= firstNumberUpperBorder; i++)
+            {
+                for (int j = 1; j <= secondNumberUpperBorder; j++)
+                {
+                    for (int k = 1; k <= thirdNumberUpperBorder; k++)
+                    {
+                        if ((i % 2 == 0 && k % 2 == 0) &&
+                        ((j == 2 || j == 3 || j == 5 || j == 7)))
+                        {
+                            Console.WriteLine($"{i} {j} {k}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

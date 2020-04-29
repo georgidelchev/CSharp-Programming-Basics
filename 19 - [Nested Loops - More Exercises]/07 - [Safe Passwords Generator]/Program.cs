41810cc2af111fdf8b4cd57bef63659789a4ae20
georgidelchev <alienguymc@gmail.com>
2020-04-29 20:02:43 +0300

    add files via upload

---
+++ 19 - [Nested Loops - More Exercises]/07 - [Safe Passwords Generator]/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SafePasswordsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int maxPasswordsCount = int.Parse(Console.ReadLine());
+
+            int firstSymbol = 35;
+            int secondSymbol = 64;
+            string passwordsCombo = String.Empty;
+            int passwordsCounter = 0;
+
+            for (int thirdSymbol = 1; thirdSymbol <= firstNumber; thirdSymbol++)
+            {
+                for (int forthSymbol = 1; forthSymbol <= secondNumber; forthSymbol++)
+                {
+                    char charFirstSymbol = (char)firstSymbol;
+                    char charSecondSymbol = (char)secondSymbol;
+                    
+                    passwordsCombo += $"" +
+                        $"{charFirstSymbol}" +
+                        $"{charSecondSymbol}" +
+                        $"{thirdSymbol}" +
+                        $"{forthSymbol}" +
+                        $"{charSecondSymbol}" +
+                        $"{charFirstSymbol}|";
+                    
+                    passwordsCounter++;
+
+                    if (passwordsCounter >= maxPasswordsCount)
+                    {
+                        Console.WriteLine(passwordsCombo);
+                        return;
+                    }
+                    
+                    firstSymbol++;
+                    secondSymbol++;
+                    
+                    if (firstSymbol > 55)
+                    {
+                        firstSymbol = 35;
+                    }
+                    if (secondSymbol > 96)
+                    {
+                        secondSymbol = 64;
+                    }
+                }
+            }
+            Console.WriteLine(passwordsCombo);
+        }
+    }
+}

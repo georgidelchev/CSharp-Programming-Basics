cfa1e2ce23193a1157782bf018b849946b7f38ca
georgidelchev <alienguymc@gmail.com>
2020-04-29 23:18:50 +0300

    add files via upload

---
+++ [Other Solutions]/14 - [Fibonacci]/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int f0 = 1;
+            int f1 = 1;
+            for (int i = 0; i < number - 1; i++)
+            {
+                int fibonacciNumber = f0 + f1;
+                f0 = f1;
+                f1 = fibonacciNumber;
+            }
+            Console.WriteLine(f1);
+        }
+    }
+}

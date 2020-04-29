aa03607850dae3e3135d757c4e721b12b02df0ea
georgidelchev <alienguymc@gmail.com>
2020-04-29 20:52:38 +0300

    add files via upload

---
+++ 19 - [Nested Loops - More Exercises]/08 - [Secret Door's Lock]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SecretDoorLock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hundredsUpperBorder = int.Parse(Console.ReadLine());
+            int dozensUpperBorder = int.Parse(Console.ReadLine());
+            int onesUpperBorder = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= hundredsUpperBorder; i++)
+            {
+                for (int j = 1; j <= dozensUpperBorder; j++)
+                {
+                    for (int k = 1; k <= onesUpperBorder; k++)
+                    {
+                        if ((k % 2 == 0) && (i % 2 == 0))
+                        {
+                            if (j == 2 || j == 3 || j == 5 || j == 7)
+                            {
+                                Console.WriteLine($"{i} {j} {k}");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

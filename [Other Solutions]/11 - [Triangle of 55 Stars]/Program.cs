859c30520d091b14abc5fc3a859c3f4822a5448e
georgidelchev <alienguymc@gmail.com>
2020-04-29 22:33:31 +0300

    add files via upload

---
+++ [Other Solutions]/11 - [Triangle of 55 Stars]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TriangleOf55Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

3f52c5f7dc2c3377d05143fbad77c35caf160f50
georgidelchev <alienguymc@gmail.com>
2020-04-29 17:27:57 +0300

    add files via upload

---
+++ 19 - [Nested Loops - More Exercises]/05 - [Challenge the Wedding]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChallengeTheWedding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mens = int.Parse(Console.ReadLine());
+            int womens = int.Parse(Console.ReadLine());
+            int maxTables = int.Parse(Console.ReadLine());
+
+            int tablesCounter = 0;
+
+            for (int i = 1; i <= mens; i++)
+            {
+                for (int j = 1; j <= womens; j++)
+                {
+                    Console.Write($"({i} <-> {j}) ");
+                    tablesCounter++;
+
+                    if (tablesCounter == maxTables)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}

287fbe1546ded5f46b4358875a6448340102f14d
georgidelchev <alienguymc@gmail.com>
2020-04-29 16:28:03 +0300

    add files via upload

---
+++ 19 - [Nested Loops - More Exercises]/02 - [Letters Combinations]/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LettersCombinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter1 = char.Parse(Console.ReadLine());
+            char letter2 = char.Parse(Console.ReadLine());
+            char missedLetter = char.Parse(Console.ReadLine());
+
+            int combinationsCounter = 0;
+            for (char i = letter1; i <= letter2; i++)
+            {
+                for (char j = letter1; j <= letter2; j++)
+                {
+                    for (char k = letter1; k <= letter2; k++)
+                    {
+                        if (i == missedLetter || j == missedLetter || k == missedLetter)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.Write($"{i}{j}{k} ");
+                            combinationsCounter++;
+                        }
+                    }
+                }
+            }
+            Console.Write(combinationsCounter);
+        }
+    }
+}

2c5521cbd12f037cad02bca30bffabf7d02a7d17
georgidelchev <alienguymc@gmail.com>
2020-04-29 13:41:43 +0300

    add files via upload

---
+++ 18 - [Nested Loops - Exercise]/01 - [Number Pyramid]/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pyramidNumber = int.Parse(Console.ReadLine());
+
+            int currentNumberInPyramid = 1;
+            bool flag = false;
+
+            for (int i = 1; i <= pyramidNumber; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (currentNumberInPyramid > pyramidNumber)
+                    {
+                        flag = true;
+                        break;
+                    }
+                    Console.Write($"{currentNumberInPyramid} ");
+                    currentNumberInPyramid++;
+                }
+                if (flag == true)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

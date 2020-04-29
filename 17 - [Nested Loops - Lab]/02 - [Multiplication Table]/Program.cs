37d2747a384d15f2c0d1021adc425b6c25001693
georgidelchev <alienguymc@gmail.com>
2020-04-29 13:21:42 +0300

    add files via upload

---
+++ 17 - [Nested Loops - Lab]/02 - [Multiplication Table]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int first = 1; first <= 10; first++)
+            {
+                for (int second = 1; second <= 10; second++)
+                {
+                    Console.WriteLine($"{first} * {second} = {first * second}");
+                }
+            }
+        }
+    }
+}

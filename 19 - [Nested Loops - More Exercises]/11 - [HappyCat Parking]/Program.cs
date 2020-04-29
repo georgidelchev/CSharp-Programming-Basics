b82a5e3d859e416464ccc71ef5940fbfd8816079
georgidelchev <alienguymc@gmail.com>
2020-04-29 21:25:17 +0300

    add files via upload

---
+++ 19 - [Nested Loops - More Exercises]/11 - [HappyCat Parking]/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HappyCatParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysCount = int.Parse(Console.ReadLine());
+            int hoursPerDay = int.Parse(Console.ReadLine());
+
+            double totalPrice = 0;
+            for (int i = 1; i <= daysCount; i++)
+            {
+                double price = 0;
+                for (int j = 1; j <= hoursPerDay; j++)
+                {
+                    if ((i % 2 == 0) && (j % 2 != 0))
+                    {
+                        price += 2.50;
+                    }
+                    else if ((i % 2 != 0) && (j % 2 == 0))
+                    {
+                        price += 1.25;
+                    }
+                    else
+                    {
+                        price += 1;
+                    }
+                }
+                totalPrice += price;
+                Console.WriteLine($"Day: {i} - {price:f2} leva");
+            }
+            Console.WriteLine($"Total: {totalPrice:f2} leva");
+        }
+    }
+}

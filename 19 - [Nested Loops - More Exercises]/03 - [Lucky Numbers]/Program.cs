b544acc6ecaeffb0d268b0c9ec9bff6db6eeb6fc
georgidelchev <alienguymc@gmail.com>
2020-04-29 16:52:50 +0300

    add files via upload

---
+++ 19 - [Nested Loops - More Exercises]/03 - [Lucky Numbers]/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int k = 1; k <= 9; k++)
+                    {
+                        for (int l = 1; l <= 9; l++)
+                        {
+                            if ((i + j) == (k + l))
+                            {
+                                if (number % (i + j) == 0)
+                                {
+                                    Console.Write($"{i}{j}{k}{l} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

20fc7388def580dbdcfc09fe38fb54c3b6a9513a
georgidelchev <alienguymc@gmail.com>
2020-04-29 23:26:29 +0300

    add files via upload

---
+++ [Other Solutions]/16 - [Number Table]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    int num = i + j + 1;
+                    if (num > number)
+                    {
+                        num = 2 * number - num;
+                    }
+                    Console.Write($"{num} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

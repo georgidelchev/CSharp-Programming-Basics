aba19b74c9562c7775a04f2b7e3b7163f0796051
georgidelchev <alienguymc@gmail.com>
2020-05-02 14:49:21 +0300

    add files via upload

---
+++ 01 - [First Steps In Coding]/06 - [Vacation books list]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pagesInCurrBook = int.Parse(Console.ReadLine());
+            double pagesPerHour = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            double totalTimeForRead = pagesInCurrBook / pagesPerHour;
+            double totalHoursPerDay = totalTimeForRead / days;
+            Console.WriteLine(totalHoursPerDay);
+        }
+    }
+}

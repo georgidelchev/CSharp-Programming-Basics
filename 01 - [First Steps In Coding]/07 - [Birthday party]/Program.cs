16ed9c6ab59fcc9e2e6c5b3f3b11f6d17b30f525
georgidelchev <alienguymc@gmail.com>
2020-05-02 14:50:34 +0300

    add files via upload

---
+++ 01 - [First Steps In Coding]/07 - [Birthday party]/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BirthdayParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double roomRent = double.Parse(Console.ReadLine());
+            double cakePrice = roomRent * 0.20;
+            double drinks = cakePrice - (cakePrice * 0.45);
+            double animator = roomRent / 3;
+            double totalPrice = roomRent + cakePrice + drinks + animator;
+            Console.WriteLine(totalPrice);
+        }
+    }
+}

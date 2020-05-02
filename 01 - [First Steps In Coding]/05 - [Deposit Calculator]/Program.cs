ebaf5cbe519c51c9a538db755ed198b382b378b4
georgidelchev <alienguymc@gmail.com>
2020-05-02 14:47:54 +0300

    add files via upload

---
+++ 01 - [First Steps In Coding]/05 - [Deposit Calculator]/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DepositCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double deposidSum = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double increasePerMonth = (deposidSum * (percent / 100)) / 12;
+            double totalSum = deposidSum + (months * increasePerMonth);
+
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}

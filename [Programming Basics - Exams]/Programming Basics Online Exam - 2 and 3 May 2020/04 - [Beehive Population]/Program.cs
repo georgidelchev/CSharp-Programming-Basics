c3084b5aca4d2eae82f4b59f90782ccc975e8111
georgidelchev <alienguymc@gmail.com>
2020-05-02 13:19:11 +0300

    add files via upload

---
+++ [Programming Basics - Exams]/Programming Basics Online Exam - 2 and 3 May 2020/04 - [Beehive Population]/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BeehivePopulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingPopulation = int.Parse(Console.ReadLine());
+            int yearsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= yearsCount; i++)
+            {
+                int newBees = startingPopulation / 10 * 2;
+                startingPopulation += newBees;
+                if (i % 5 == 0)
+                {
+                    int migratedBees = startingPopulation / 50 * 5;
+                    startingPopulation -= migratedBees;
+                }
+                int deadBees = startingPopulation / 20 * 2;
+                startingPopulation -= deadBees;
+            }
+            Console.WriteLine($"Beehive population: {startingPopulation}");
+        }
+    }
+}

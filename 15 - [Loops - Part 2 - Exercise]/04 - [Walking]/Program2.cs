87d0b980295148b25efac94524de291b4db335c4
georgidelchev <alienguymc@gmail.com>
2020-05-07 14:37:06 +0300

    add files via upload

---
+++ 15 - [Loops - Part 2 - Exercise]/04 - [Walking]/Program2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSteps = 0;
+            int homeSteps = 0;
+
+            while (true)
+            {
+                string stepsCounter = Console.ReadLine();
+                if (stepsCounter == "Going home")
+                {
+                    homeSteps = int.Parse(Console.ReadLine());
+                    totalSteps += homeSteps;
+                    if (totalSteps < 10000)
+                    {
+                        Console.WriteLine($"{10000 - totalSteps} more steps to reach goal.");
+                    }
+                    else if (totalSteps >= 10000)
+                    {
+                        Console.WriteLine($"Goal reached! Good job!");
+                        Console.WriteLine($"{totalSteps - 10000} steps over the goal!");
+                        break;
+                    }
+                    break;
+                }
+                int steps = int.Parse(stepsCounter);
+                totalSteps += steps;
+                if (totalSteps >= 10000)
+                {
+                    Console.WriteLine($"Goal reached! Good job!");
+                    Console.WriteLine($"{totalSteps - 10000} steps over the goal!");
+                    break;
+                }
+            }
+        }
+    }
+}

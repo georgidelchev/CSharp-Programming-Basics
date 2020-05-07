9fa11e36127183a2034aa3206df55113c834eec7
georgidelchev <alienguymc@gmail.com>
2020-05-07 14:21:30 +0300

    add files via upload

---
+++ 14 - [Loops - Part 2 - Lab]/06 - [Max & MinNumber]/Max1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.MinValue;
+            string input = Console.ReadLine();
+            while (input != "Stop")
+            {
+                int numbers = int.Parse(input);
+                if (numbers > maxNumber)
+                {
+                    maxNumber = numbers;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}

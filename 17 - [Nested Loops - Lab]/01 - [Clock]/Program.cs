2722f4a0f4eb1508e0137650f2d7a336d4e965a0
georgidelchev <alienguymc@gmail.com>
2020-05-07 14:43:11 +0300

    add files via upload

---
+++ 17 - [Nested Loops - Lab]/01 - [Clock]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClockS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int h = 0; h <= 23; h++)
+            {
+                for (int m = 0; m <= 59; m++)
+                {
+                    Console.WriteLine($"{h}:{m}");
+                }
+            }
+        }
+    }
+}

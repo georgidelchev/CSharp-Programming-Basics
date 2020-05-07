b389d9c82a0530ee02f1e1428a46ae8bbddd5c70
georgidelchev <alienguymc@gmail.com>
2020-05-07 14:23:02 +0300

    add files via upload

---
+++ 14 - [Loops - Part 2 - Lab]/06 - [Max & MinNumber]/Min1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = int.MaxValue;
+            string input = Console.ReadLine();
+            while (input != "Stop")
+            {
+                int numbers = int.Parse(input);
+                if (numbers < minNumber)
+                {
+                    minNumber = numbers;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}

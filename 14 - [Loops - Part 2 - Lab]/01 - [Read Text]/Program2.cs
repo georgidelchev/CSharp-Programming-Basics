b4ee0097b4817038a3251052995c86c23c98a8b2
georgidelchev <alienguymc@gmail.com>
2020-05-07 14:08:15 +0300

    add files via upload

---
+++ 14 - [Loops - Part 2 - Lab]/01 - [Read Text]/Program2.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace ReadText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            while (word != "Stop")
+            {
+                Console.WriteLine(word);
+                word = Console.ReadLine();
+            }
+        }
+    }
+}

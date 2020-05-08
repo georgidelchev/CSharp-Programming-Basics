d58693f5fbaaf8ecc4c819619fa29ad4edd6562d
georgidelchev <alienguymc@gmail.com>
2020-05-08 18:32:26 +0300

    add files via upload

---
+++ [Other Solutions]/26 - [Stop Number]/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StopNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int stoppingNumber = int.Parse(Console.ReadLine());
+
+            for (int i = M; i >= N; i--)
+            {
+                if ((i % 2 == 0) && (i % 3 == 0))
+                {
+                    if (i == stoppingNumber)
+                    {
+                        break;
+                    }
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}

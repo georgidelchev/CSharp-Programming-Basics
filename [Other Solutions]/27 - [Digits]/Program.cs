ed873916d46b6ebc784f46a61319f94e0730ed19
georgidelchev <alienguymc@gmail.com>
2020-05-08 19:05:42 +0300

    add files via upload

---
+++ [Other Solutions]/27 - [Digits]/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int copyOfTheNum = number;
+            int thirdDigit = copyOfTheNum % 10;
+            copyOfTheNum /= 10;
+            int secondDigit = copyOfTheNum % 10;
+            copyOfTheNum /= 10;
+            int firstDigit = copyOfTheNum % 10;
+
+            for (int i = 0; i < firstDigit + secondDigit; i++)
+            {
+                for (int j = 0; j < firstDigit + thirdDigit; j++)
+                {
+                    if (number % 5 == 0)
+                    {
+                        number -= firstDigit;
+                    }
+                    else if (number % 3 == 0)
+                    {
+                        number -= secondDigit;
+                    }
+                    else
+                    {
+                        number += thirdDigit;
+                    }
+                    Console.Write($"{number} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

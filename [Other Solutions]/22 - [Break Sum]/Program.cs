71072468ce0b58ce60d96e023cca85ee72037a6c
georgidelchev <alienguymc@gmail.com>
2020-05-08 17:55:06 +0300

    add files via upload

---
+++ [Other Solutions]/22 - [Break Sum]/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BreakSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = false;
+            for (int i = 1; i <= 3; i++)
+            {
+                if (flag == false)
+                {
+                    for (int j = 3; j >= 1; j--)
+                    {
+                        if (i + j == 2)
+                        {
+                            flag = true;
+                            break;
+                        }
+                        Console.WriteLine($"{i} {j}");
+                    }
+                }
+
+            }
+        }
+    }
+}

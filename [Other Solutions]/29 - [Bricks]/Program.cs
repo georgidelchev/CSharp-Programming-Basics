326caf54fcd18397a5b931ab8e90204f7ae240aa
georgidelchev <alienguymc@gmail.com>
2020-05-08 19:19:35 +0300

    add files via upload

---
+++ [Other Solutions]/29 - [Bricks]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bricksCount = int.Parse(Console.ReadLine());
+            int workersCount = int.Parse(Console.ReadLine());
+            int cartVolume = int.Parse(Console.ReadLine());
+
+            int totalBricks = workersCount * cartVolume;
+            double courses = Math.Ceiling((double)bricksCount / totalBricks);
+            Console.WriteLine(courses);
+        }
+    }
+}

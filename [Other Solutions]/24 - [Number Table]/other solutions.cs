228bc4206cd4203887385ded56575514ced5bfd1
georgidelchev <alienguymc@gmail.com>
2020-05-08 18:03:30 +0300

    add files via upload

---
+++ [Other Solutions]/24 - [Number Table]/other solutions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NumberTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int num = i + j + 1;
+                    if (num > n)
+                    {
+                        num = 2 * n - num;
+                    }
+                    Console.Write($"{num} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

101b7fb14f69d5cd94de34e7bc5fb7b7ecfa3282
georgidelchev <alienguymc@gmail.com>
2020-05-08 19:33:10 +0300

    add files via upload

---
+++ [Other Solutions]/30 - [Point on Segment]/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PointOnSegment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstPoint = int.Parse(Console.ReadLine());
+            int secondPoint = int.Parse(Console.ReadLine());
+            int point = int.Parse(Console.ReadLine());
+
+            int first = Math.Min(firstPoint, secondPoint);
+            int second = Math.Max(firstPoint, secondPoint);
+
+            int left = Math.Abs(first - point);
+            int right = Math.Abs(second - point);
+            int min = Math.Min(left, right);
+
+            if (point >= first && point <= second)
+            {
+                Console.WriteLine("in");
+            }
+            else
+            {
+                Console.WriteLine("out");
+            }
+
+            Console.WriteLine(min);
+        }
+    }
+}

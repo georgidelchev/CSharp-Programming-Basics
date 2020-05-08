c8984463266966813d135d97fefe36538eb788a3
georgidelchev <alienguymc@gmail.com>
2020-05-08 19:14:10 +0300

    add files via upload

---
+++ [Other Solutions]/28 - [Triangle Area]/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1 = int.Parse(Console.ReadLine());
+            int y1 = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            int x3 = int.Parse(Console.ReadLine());
+            int y3 = int.Parse(Console.ReadLine());
+
+            int a = Math.Abs(x2 - x3);
+            int h = Math.Abs(y2 - y1);
+            double area = (double)a * h / 2;
+            Console.WriteLine(area);
+        }
+    }
+}

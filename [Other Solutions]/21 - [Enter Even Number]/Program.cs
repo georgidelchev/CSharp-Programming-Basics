6c457fb828dda800803dc6d1f2dac654a24ff6c3
georgidelchev <alienguymc@gmail.com>
2020-05-08 17:49:31 +0300

    add files via upload

---
+++ [Other Solutions]/21 - [Enter Even Number]/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EnterEvenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while (true)
+            {
+                Console.Write("Enter even number: ");
+                number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    break;
+                }
+                Console.WriteLine("The number is not even.");
+            }
+            Console.WriteLine($"Even number entered: {number}");
+        }
+    }
+}

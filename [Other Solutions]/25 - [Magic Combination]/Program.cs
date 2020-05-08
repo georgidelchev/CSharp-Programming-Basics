73ebd9e51cef67ade03fc5e68fd0ac1069e6eddb
georgidelchev <alienguymc@gmail.com>
2020-05-08 18:24:17 +0300

    add files via upload

---
+++ [Other Solutions]/25 - [Magic Combination]/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MagicCombination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int magicNumber = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int k = 1; k <= 9; k++)
+                    {
+                        for (int l = 1; l <= 9; l++)
+                        {
+                            for (int m = 1; m <= 9; m++)
+                            {
+                                for (int n = 1; n <= 9; n++)
+                                {
+                                    if (i * j * k * l * m * n == magicNumber)
+                                    {
+                                        Console.Write($"{i}{j}{k}{l}{m}{n} ");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}

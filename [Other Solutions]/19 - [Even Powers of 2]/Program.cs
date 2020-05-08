d1cebc614c2aa9f797f1b6e0870fb798ed38c29a
georgidelchev <alienguymc@gmail.com>
2020-05-08 17:39:15 +0300

    add files via upload

---
+++ [Other Solutions]/19 - [Even Powers of 2]/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EvenPowersOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int start = 1;
+            for (int i = 0; i <= num; i += 2)
+            {
+                Console.WriteLine(start);
+                start = 2 * 2 * start;
+            }
+        }
+    }
+}

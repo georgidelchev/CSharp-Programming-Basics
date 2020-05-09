1a296df2ac8e9d5771f9da0c7150bdcab04fc20e
georgidelchev <alienguymc@gmail.com>
2020-05-09 16:01:04 +0300

    add files via upload

---
+++ [Programming Basics - Exams]/Programming Basics Online 2020 Exams/Programming Basics Online Exam - 9 and 10 May 2020/05 - [Exam Preparation]/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studsCount = int.Parse(Console.ReadLine());
+            int tasksCount = int.Parse(Console.ReadLine());
+            int energy = int.Parse(Console.ReadLine());
+
+            int totalTasks = 0;
+            int totalQuestions = 0;
+            while (energy >= 1)
+            {
+                totalTasks += tasksCount;
+                energy += 2 * tasksCount;
+                studsCount -= tasksCount;
+                energy -= (studsCount * 2) * 3;
+                totalQuestions += studsCount * 2;
+                if (studsCount < 10)
+                {
+                    Console.WriteLine("The students are too few!");
+                    Console.WriteLine($"Problems solved: {totalTasks}");
+                    break;
+                }
+                studsCount += (studsCount / 10) * 1;
+            }
+            if (energy <= 0)
+            {
+                Console.WriteLine("The trainer is too tired!");
+                Console.WriteLine($"Questions answered: {totalQuestions}");
+            }
+        }
+    }
+}

b5599b2a443778e01663fec2b87b14dfb2c65bcc
georgidelchev <alienguymc@gmail.com>
2020-05-09 15:55:14 +0300

    add files via upload

---
+++ [Programming Basics - Exams]/Programming Basics Online 2020 Exams/Programming Basics Online Exam - 9 and 10 May 2020/02 - [Exam Points]/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int task = int.Parse(Console.ReadLine());
+            int points = int.Parse(Console.ReadLine());
+            string course = Console.ReadLine();
+
+            double totPoints = 0;
+            if (course == "Basics")
+            {
+                if (task == 1)
+                {
+                    totPoints = points * 0.08;
+                    totPoints -= totPoints * 0.20;
+                }
+                else if (task == 2)
+                {
+                    totPoints = points * 0.09;
+                }
+                else if (task == 3)
+                {
+                    totPoints = points * 0.09;
+                }
+                else if (task == 4)
+                {
+                    totPoints = points * 0.10;
+                }
+            }
+            else if (course == "Fundamentals")
+            {
+                if (task == 1)
+                {
+                    totPoints = points * 0.11;
+                }
+                else if (task == 2)
+                {
+                    totPoints = points * 0.11;
+                }
+                else if (task == 3)
+                {
+                    totPoints = points * 0.12;
+                }
+                else if (task == 4)
+                {
+                    totPoints = points * 0.13;
+                }
+            }
+            else if (course == "Advanced")
+            {
+                if (task == 1)
+                {
+                    totPoints = points * 0.14;
+                }
+                else if (task == 2)
+                {
+                    totPoints = points * 0.14;
+                }
+                else if (task == 3)
+                {
+                    totPoints = points * 0.15;
+                }
+                else if (task == 4)
+                {
+                    totPoints = points * 0.16;
+                }
+                totPoints += totPoints * 0.20;
+            }
+            Console.WriteLine($"Total points: {totPoints:f2}");
+        }
+    }
+}

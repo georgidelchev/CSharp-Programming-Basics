b1f8d60c6d336a8b36b593f163e71652ecf1500e
georgidelchev <alienguymc@gmail.com>
2020-05-09 15:51:29 +0300

    add

---
+++ [Programming Basics - Exams]/Programming Basics Online 2020 Exams/[Exam Preparation]/01 - [Savings]/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int incomePerMonth = int.Parse(Console.ReadLine());
+            int monthsCount = int.Parse(Console.ReadLine());
+            double neededMoney = double.Parse(Console.ReadLine());
+
+            double moneySheNeeds = incomePerMonth * 0.30;
+            double savedMoneyPerMonth = incomePerMonth - (neededMoney + moneySheNeeds);
+            double totalSavedMoney = monthsCount * savedMoneyPerMonth;
+
+            Console.WriteLine($"She can save {(savedMoneyPerMonth / incomePerMonth * 1.00) * 100:f2}%");
+            Console.WriteLine($"{totalSavedMoney:f2}");
+        }
+    }
+}

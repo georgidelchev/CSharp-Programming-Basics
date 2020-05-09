672a38a1bf6e4788fa557f65071a1e3ac0d389ec
georgidelchev <alienguymc@gmail.com>
2020-05-09 15:59:52 +0300

    add files via upload

---
+++ [Programming Basics - Exams]/Programming Basics Online 2020 Exams/Programming Basics Online Exam - 9 and 10 May 2020/04 - [Exam Retention]/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int studsCount = int.Parse(Console.ReadLine());
+            int seasonsCount = int.Parse(Console.ReadLine());
+            double newStuds = 0;
+            double continues = 0;
+
+            double examForSecondCourse = 0;
+            for (int i = 1; i <= seasonsCount; i++)
+            {
+                double examForFirstCourse = Math.Ceiling(studsCount * 0.9);
+                examForSecondCourse = Math.Ceiling(examForFirstCourse * 0.9);
+                continues = Math.Ceiling(examForSecondCourse * 0.8);
+
+                if (i % 3 == 0)
+                {
+                    newStuds = Math.Ceiling(continues * 0.15);
+                }
+                else
+                {
+                    newStuds = Math.Ceiling(continues * 0.05);
+                }
+                studsCount = (int)newStuds + (int)continues;
+            }
+
+            Console.WriteLine($"Students: {continues + newStuds}");
+        }
+    }
+}

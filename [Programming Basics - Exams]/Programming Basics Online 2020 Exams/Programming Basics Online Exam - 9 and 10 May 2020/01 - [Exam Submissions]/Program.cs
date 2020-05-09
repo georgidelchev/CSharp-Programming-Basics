fd9974c99785609830c211180616afe856d51877
georgidelchev <alienguymc@gmail.com>
2020-05-09 15:54:00 +0300

    add files via upload

---
+++ [Programming Basics - Exams]/Programming Basics Online 2020 Exams/Programming Basics Online Exam - 9 and 10 May 2020/01 - [Exam Submissions]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExamSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numStudents = int.Parse(Console.ReadLine());
+            int numTasks = int.Parse(Console.ReadLine());
+
+            double tasks = Math.Ceiling(numTasks * 2.8) * numStudents;
+
+            int addSolutions = numTasks / 3 * numStudents;
+
+            double totalSolutions = tasks + addSolutions;
+
+            double memory = Math.Ceiling(totalSolutions / 10) * 5;
+
+            Console.WriteLine($"{memory} KB needed");
+            Console.WriteLine($"{totalSolutions} submissions");
+        }
+    }
+}

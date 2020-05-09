dedbb83697a1577536b605bb65b51ec8afbfdfaa
georgidelchev <alienguymc@gmail.com>
2020-05-09 15:01:56 +0300

    add files via upload

---
+++ [Programming Basics - Exams]/Programming Basics Online 2020 Exams/Programming Basics Online Exam - 2 and 3 May 2020/05 - [Beehive Defense]/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BeehiveDefense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int beesCount = int.Parse(Console.ReadLine());
+            int bearHealth = int.Parse(Console.ReadLine());
+            int bearAttack = int.Parse(Console.ReadLine());
+
+            int beesAttack = 5;
+            while (bearHealth > 0)
+            {
+                beesCount = beesCount - bearAttack;
+                bearHealth -= beesCount * beesAttack;
+                if (beesCount < 100)
+                {
+                    if (beesCount < 0)
+                    {
+                        beesCount = 0;
+                    }
+                    Console.WriteLine($"The bear stole the honey! Bees left {beesCount}.");
+                    break;
+                }
+            }
+            if (bearHealth <= 0)
+            {
+                Console.WriteLine($"Beehive won! Bees left {beesCount}.");
+            }
+        }
+    }
+}

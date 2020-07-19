a54cabcde1f1f86bc57dba998cc796db312e44c3
georgidelchev <alienguymc@gmail.com>
2020-07-19 17:05:30 +0300

    add

---
+++ [Programming Basics - Exams]/Programming Basics Online 2020 Exams/Programming Basics Online Exam - 18 and 19 July 2020/01 - [Agency Profit]/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AgencyProfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string companyName = Console.ReadLine();
+            int adultsTicketsCount = int.Parse(Console.ReadLine());
+            int childsTicketsCount = int.Parse(Console.ReadLine());
+            double adultsTicketsPrice = double.Parse(Console.ReadLine());
+            double tax = double.Parse(Console.ReadLine());
+
+            double childTicketsPrice = adultsTicketsPrice - (adultsTicketsPrice * 0.70) + tax;
+            adultsTicketsPrice += tax;
+
+            double totalPrice = adultsTicketsCount * adultsTicketsPrice + childsTicketsCount * childTicketsPrice;
+            double profit = totalPrice * 0.20;
+
+            Console.WriteLine($"The profit of your agency from {companyName} tickets is {profit:f2} lv.");
+        }
+    }
+}

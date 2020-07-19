647c9ff42072f6f46b20cc5d248f5ee148f27d8a
georgidelchev <alienguymc@gmail.com>
2020-07-19 16:58:05 +0300

    add

---
+++ [Programming Basics - Exams]/Programming Basics Online 2020 Exams/Programming Basics Online Exam - 18 and 19 July 2020/05 - [Best Player]/Programming Basics - Exams.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName;
+
+            string bestPlayerName = string.Empty;
+            int bestGoals = 0;
+
+            while ((playerName = Console.ReadLine()) != "END")
+            {
+                int goals = int.Parse(Console.ReadLine());
+
+                if (goals > bestGoals)
+                {
+                    bestGoals = goals;
+                    bestPlayerName = playerName;
+                }
+
+                if (goals >= 10)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{bestPlayerName} is the best player!");
+
+            if (bestGoals >= 3)
+            {
+                Console.WriteLine($"He has scored {bestGoals} goals and made a hat-trick !!!");
+            }
+            else
+            {
+                Console.WriteLine($"He has scored {bestGoals} goals.");
+            }
+        }
+    }
+}

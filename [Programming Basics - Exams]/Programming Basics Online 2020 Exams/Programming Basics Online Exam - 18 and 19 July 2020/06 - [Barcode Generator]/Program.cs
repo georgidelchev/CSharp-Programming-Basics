1dc7c3d15feb46e6b5d456e3df4478c637ec6fe6
georgidelchev <alienguymc@gmail.com>
2020-07-19 16:40:13 +0300

    add

---
+++ [Programming Basics - Exams]/Programming Basics Online 2020 Exams/Programming Basics Online Exam - 18 and 19 July 2020/06 - [Barcode Generator]/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BarcodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string start = Console.ReadLine();
+            string end = Console.ReadLine();
+
+            for (int j = int.Parse(start[0].ToString()); j <= int.Parse(end[0].ToString()); j++)
+            {
+                for (int k = int.Parse(start[1].ToString()); k <= int.Parse(end[1].ToString()); k++)
+                {
+                    for (int l = int.Parse(start[2].ToString()); l <= int.Parse(end[2].ToString()); l++)
+                    {
+                        for (int m = int.Parse(start[3].ToString()); m <= int.Parse(end[3].ToString()); m++)
+                        {
+                            if ((j % 2 != 0) && (k % 2 != 0) && (l % 2 != 0) && (m % 2 != 0))
+                            {
+                                Console.Write($"{j}{k}{l}{m} ");
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
